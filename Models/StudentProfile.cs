c2f29997c99f0cb4b51ed42b3740dbe4eadc1c0c
Plexukas <algirdasleonardas@gmail.com>
2024-03-10 19:04:01 +0200

    Defined StudentProfile

---
+++ Models/StudentProfile.cs
@@ -0,0 +1,10 @@
+namespace TutorAPI.Models
+{
+    public class StudentProfile : Profile
+    {
+        public string Grade { get; set; } = string.Empty;
+        public List<Subject> Subjects { get; set; } = new List<Subject>();
+        public List<Session> Sessions { get; set; } = new List<Session>();
+        
+    }
+}
