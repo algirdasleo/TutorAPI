db9fe5f67ccf0b054cd6ddda76cfb37786307ba3
Plexukas <algirdasleonardas@gmail.com>
2024-03-10 19:04:06 +0200

    Defined Subject

---
+++ Models/Subject.cs
@@ -0,0 +1,9 @@
+namespace TutorAPI.Models
+{
+    public class Subject
+    {
+        public int SubjectId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
