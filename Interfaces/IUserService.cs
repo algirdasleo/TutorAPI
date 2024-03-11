c89fb5f85017e21b13ecc6140f755f0fe8ee1e7a
Plexukas <algirdasleonardas@gmail.com>
2024-03-11 20:37:53 +0200

    UserService Interface

---
+++ Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+using TutorAPI.Models;
+using System.Threading.Tasks;
+
+namespace TutorAPI.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> CreateUserAsync(User user);
+        Task<User> UpdateUserAsync(User user);
+        Task<bool> DeleteUserAsync(int id);
+        Task<User> GetUserByIdAsync(int id);
+        Task<User> GetUserByUsernameAsync(string username);
+        Task<User> AuthenticateUserAsync(string username, string password);
+        Task<bool> IsUsernameTakenAsync(string username);
+        Task<bool> IsEmailTakenAsync(string email);
+        Task<bool> IsPhoneNumberTakenAsync(string phoneNumber);
+    }
+}
