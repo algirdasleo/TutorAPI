cc2b44b7823d134a86b724ba70bf7eb5fa24f527
Plexukas <algirdasleonardas@gmail.com>
2024-03-13 15:01:20 +0200

    Changed duration from string to int

--- Models/Session.cs
+++ Models/Session.cs
@@ -9,7 +9,7 @@ namespace TutorAPI.Models
         public StudentProfile StudentProfile { get; set; } = null!; 
         public string Date { get; set; } = string.Empty;            // yyyy-mm-dd
         public string Time { get; set; } = string.Empty;            // 24 hour format
-        public string Duration { get; set; } = string.Empty;        // 30, 60, 90, 120 mins
+        public int Duration { get; set; }                           // 30, 60, 90, 120 mins
         public string Location { get; set; } = string.Empty;        // Online, In-Person
         public string Status { get; set; } = string.Empty;          // Planned, Confirmed, Completed, Cancelled
     }
