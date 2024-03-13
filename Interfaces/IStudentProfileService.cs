baa193c6af9672986de6c6e2ac113ee91aa50b25
Plexukas <algirdasleonardas@gmail.com>
2024-03-13 15:59:21 +0200

    updated profile interfaces

--- Interfaces/IStudentProfileService.cs
+++ Interfaces/IStudentProfileService.cs
@@ -4,8 +4,8 @@ namespace TutorAPI.Interfaces
     public interface IStudentProfileService
     {
         Task<List<StudentProfile>> GetStudentProfilesAsync();
-        Task<StudentProfile> GetStudentProfileByProfileId(int profileId);
-        Task<StudentProfile> GetStudentProfileByUserId(int userId);
+        Task<StudentProfile?> GetStudentProfileByProfileId(int profileId);
+        Task<StudentProfile?> GetStudentProfileByUserId(int userId);
         Task<int> AddStudentProfileAsync(StudentProfile studentProfile);
         Task<bool> UpdateStudentProfileAsync(StudentProfile studentProfile);
         Task<bool> DeleteStudentProfileAsync(int profileId);
--- Interfaces/ITutorProfileService.cs
+++ Interfaces/ITutorProfileService.cs
@@ -1,13 +1,12 @@
 using TutorAPI.Models;
-
 namespace TutorAPI.Interfaces
 {
     public interface ITutorProfileService
     {
         Task<List<TutorProfile>> GetTutorProfilesAsync();
-        Task<TutorProfile> GetTutorProfileByProfileId(int profileId);
-        Task<TutorProfile> GetTutorProfileByUserId(int userId);
-        Task<TutorProfile> AddTutorProfileAsync(TutorProfile tutorProfile);
+        Task<TutorProfile?> GetTutorProfileByProfileId(int profileId);
+        Task<TutorProfile?> GetTutorProfileByUserId(int userId);
+        Task<int> AddTutorProfileAsync(TutorProfile tutorProfile);
         Task<bool> UpdateTutorProfileAsync(TutorProfile tutorProfile);
         Task<bool> DeleteTutorProfileAsync(int profileId);
     }
