7914e9dc9b1cb090f4d624cb75a35ce5f4aa778c
Plexukas <algirdasleonardas@gmail.com>
2024-03-13 17:48:19 +0200

    Implemented UserController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,62 @@
+using TutorAPI.Interfaces;
+using TutorAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Tutor.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userService.GetAllUsersAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            if (user == null)
+                return NotFound();
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(User user)
+        {
+            var userId = await _userService.CreateUserAsync(user);
+            var actionName = nameof(GetUserById);
+            var routeValues = new { id = userId };
+            return CreatedAtAction(actionName, routeValues, user);
+        }
+        
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser(User user)
+        {
+            var changedRows = await _userService.UpdateUserAsync(user);
+            if (changedRows == false)
+                return NotFound();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var deleted = await _userService.DeleteUserAsync(id);
+            if (deleted == false)
+                return NotFound();
+            return NoContent();
+        }
+    }
+}
