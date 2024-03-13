2c416e94a3a52ac3312a2501f31345eb0f05104d
Plexukas <algirdasleonardas@gmail.com>
2024-03-13 15:41:40 +0200

    Changed return types

--- Interfaces/IUserService.cs
+++ Interfaces/IUserService.cs
@@ -5,8 +5,8 @@ namespace TutorAPI.Interfaces
 {
     public interface IUserService
     {
-        Task<IEnumerable<User>> GetAllUsersAsync();
-        Task<User> GetUserByIdAsync(int id);
+        Task<List<User>> GetAllUsersAsync();
+        Task<User?> GetUserByIdAsync(int id);
         Task<User> CreateUserAsync(User user);
         Task<int> UpdateUserAsync(User user);
         Task<bool> DeleteUserAsync(int id);
