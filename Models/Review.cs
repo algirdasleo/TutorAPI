eff56eb8d4b520c503437f69073596eeb4c6e621
Plexukas <algirdasleonardas@gmail.com>
2024-03-13 10:10:06 +0200

    added createdAt element

--- Models/Review.cs
+++ Models/Review.cs
@@ -7,5 +7,6 @@ namespace TutorAPI.Models
         public string Comment { get; set; } = string.Empty;
         public int TutorId { get; set; }
         public int StudentId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
