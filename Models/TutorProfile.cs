146b66b8b483b054c45f380486b8a08e1ffa002a
Plexukas <algirdasleonardas@gmail.com>
2024-03-13 13:33:03 +0200

    changed return types

--- Models/TutorProfile.cs
+++ Models/TutorProfile.cs
@@ -3,7 +3,7 @@ namespace TutorAPI.Models
     public class TutorProfile : Profile
     {
         public string Description { get; set; } = string.Empty;
-        public string Price { get; set; } = string.Empty;
+        public double Price { get; set; }
         public string Qualifications { get; set; } = string.Empty;
         public List<Subject> Subjects { get; set; } = new List<Subject>();
         public List<Session> Sessions { get; set; } = new List<Session>();
