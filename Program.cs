0e156ca4798f5ce543d81c74f73803078ab51a4f
Plexukas <algirdasleonardas@gmail.com>
2024-03-14 18:59:06 +0200

    DI included

--- Program.cs
+++ Program.cs
@@ -7,22 +7,18 @@ using TutorAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddScoped<IDatabaseService, DatabaseService>();
-builder.Services.AddScoped<ITutorProfileService, TutorProfileService>();
-builder.Services.AddScoped<IStudentProfileService, StudentProfileService>();
+builder.Services.AddScoped<IProfileService, ProfileService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ISubjectService, SubjectService>();
 builder.Services.AddScoped<ISessionService, SessionService>();
 builder.Services.AddScoped<IReviewService, ReviewService>();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
