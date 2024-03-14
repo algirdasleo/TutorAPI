8f70717f06a163f968c0211fa89d5eb005663524
Plexukas <algirdasleonardas@gmail.com>
2024-03-14 18:56:45 +0200

    Deleted to simplify DB

--- Models/StudentProfile.cs
+++
@@ -1,9 +0,0 @@
-namespace TutorAPI.Models
-{
-    public class StudentProfile : Profile
-    {
-        public string Grade { get; set; } = string.Empty;
-        public List<Subject> Subjects { get; set; } = new List<Subject>();
-        public List<Session> Sessions { get; set; } = new List<Session>();
-    }
-}
