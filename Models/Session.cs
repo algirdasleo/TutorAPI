0b017c10543ed1b1c3b2f35b20fee68ca6de1cac
Plexukas <algirdasleonardas@gmail.com>
2024-03-19 16:41:53 +0200

    Added DateTime instead of strings

--- Models/Session.cs
+++ Models/Session.cs
@@ -5,8 +5,7 @@ namespace TutorAPI.Models
         public int? SessionId { get; set; }
         public int? TutorProfileId { get; set; }
         public int? StudentProfileId { get; set; }
-        public string Date { get; set; } = string.Empty;            // yyyy-mm-dd
-        public string Time { get; set; } = string.Empty;            // 24 hour format
+        public DateTime StartTime { get; set; }
         public int Duration { get; set; }                           // 30, 60, 90, 120 mins
         public string? Location { get; set; } = string.Empty;        // Online, In-Person
         public string Status { get; set; } = string.Empty;          // Planned, Confirmed, Completed, Cancelled
